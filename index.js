deb610e2ed93756a5c77260bbbde27252a5bf4d3
dmitridev <dmitrit.dev@gmail.com>
2021-05-10 21:33:47 +0300

    del

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const objectId = require("mongodb").ObjectId;
-const session = require('express-session');
-
 require('dotenv').config();
 
 const express = require('express');
+const router = express.Router();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const objectId = require("mongodb").ObjectId;
+
 const app = express();
 const port = 8080;
 
 const connection = getConnection();
 
 function getConnection() {
-  const user = process.env.DB_USER;
-  const db = process.env.DB_NAME;
-  const connection = process.env.DB_CONNECTION;
-  const password = process.env.DB_PASSWORD;
-  const local = process.env.local;
 
+  const {DB_USER:user,DB_NAME:db,DB_CONNECTION:connection,DB_PASSWORD:password,local} = process.env
   if (local) return `mongodb://localhost:27017`
 
   const conn = `mongodb+srv://${user}:${password}@${connection}/${db}?retryWrites=true&w=majority`
+
   console.log(conn);
   return conn;
 }
@@ -30,27 +31,30 @@ const mongoOptions = {
   useNewUrlParser: true
 };
 
-
-app.use(session({ secret: 'sdwerqer', saveUninitialized: true, resave: true, cookie: { secure: true } }));
+app.use(jwt({ secret : process.env.SECRET }));
 
 app.use(bodyParser.json());
 
-app.get("/api/notes/list", (req, res) => {
+router.get("/api/notes/list", (req, res) => {
   let folder = req.query.folder;
+  let userID = req.query.userID;
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
     const db = client.db('regulars');
     let collection = null;
     if (folder) {
-      collection = await db.collection('notes').find({ folderId: folder }).toArray();
+      if(userID)
+        collection = await db.collection('notes').find({ userID: userId }).toArray()
+      else
+        collection = await db.collection('notes').find({ folderId: folder }).toArray();
     } else {
-      collection = await db.collection('notes').find({}).toArray();
+      collection = await db.collection('notes').find({ userId }).toArray();
     }
     res.send(collection);
   })
 });
 
-app.put('/api/notes/:id', (req, res) => {
+router.put('/api/notes/:id', (req, res) => {
   const body = req.body
   const id = req.params.id;
   const client = new MongoClient(connection, mongoOptions);
@@ -65,7 +69,7 @@ app.put('/api/notes/:id', (req, res) => {
   })
 })
 
-app.get("/api/notes/:id", (req, res) => {
+router.get("/api/notes/:id", (req, res) => {
   const id = req.params.id;
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
@@ -79,7 +83,7 @@ app.get("/api/notes/:id", (req, res) => {
   })
 })
 
-app.post("/api/notes/", (req, res) => {
+router.post("/api/notes/", (req, res) => {
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
     try {
@@ -93,7 +97,7 @@ app.post("/api/notes/", (req, res) => {
   })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   const client = new MongoClient(connection, mongoOptions);
   client.connect((err) => {
@@ -108,7 +112,7 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 })
 
-app.get('/api/notes/count', (req, res) => {
+router.get('/api/notes/count', (req, res) => {
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
     try {
@@ -122,7 +126,7 @@ app.get('/api/notes/count', (req, res) => {
   });
 })
 
-app.get('/api/folders/', (req, res) => {
+router.get('/api/folders/', (req, res) => {
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
     try {
@@ -134,7 +138,8 @@ app.get('/api/folders/', (req, res) => {
     }
   })
 })
-app.post('/api/folders/:name', (req, res) => {
+
+router.post('/api/folders/:name', (req, res) => {
   const name = req.params.name;
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
@@ -148,7 +153,7 @@ app.post('/api/folders/:name', (req, res) => {
   })
 });
 
-app.delete('/api/folders/:name', (req, res) => {
+router.delete('/api/folders/:name', (req, res) => {
   const name = req.params.name;
   const client = new MongoClient(connection, mongoOptions);
   client.connect(async (err) => {
@@ -162,7 +167,30 @@ app.delete('/api/folders/:name', (req, res) => {
   });
 })
 
+router.post('/api/user/login', (req,res) => {
+  const session = req.session;
+  
+  const {login} = req.body;
+  session.user = login
+  res.send(session);
+});
 
+app.post('/api/user/register',(req,res) => {
+  const {login,password} = req.body;
+  const client = new MongoClient(connection,mongoOptions);
+  client.connect(async (err) => {
+    try{
+      const db = client.db('regulars');
+      const result = await db.collection('users').insertOne({login,password});
+      res.sendStatus(200);
+    }catch(err){
+      res.send(err);
+    }
+  });
+});
+
+
+app.use('/',router);
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
 });
